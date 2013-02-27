56e8d5dffea840990caf2b27e14ae645854e9f2e
Zaid <zaid.wadud@bjitgroup.com>
2013-02-27 12:06:07 +0600

    New File

---
+++ Web1/src/org/util/Test1.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package org.util;
+
+/**
+ * @author Wadud
+ *
+ */
+public class Test1 {
+
+	/**
+	 * 
+	 */
+	public Test1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
